b23457dfe1f230b8623e07a32befd442e12c10bf
Ellie Tew <ellietew@Ellies-MacBook-Air.local>
2022-04-20 23:08:29 -0500

    clock out

--- Resources/scripts/clockInAndOut.js
+++ Resources/scripts/clockInAndOut.js
@@ -13,6 +13,24 @@ function displayEmpID(){
     document.getElementById("UserLink").innerHTML=html;
 }
 
+function GetClock(){
+    var d=new Date();
+    var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds();
+    if(nmin<=9) nmin="0"+nmin;
+    if(nsec<=9) nsec="0"+nsec;
+    
+    var clocktext=""+nhour+":"+nmin+":"+nsec+"";
+    document.getElementById('clockbox').innerHTML=clocktext;
+    
+} 
+
+function toggle(){
+    var blur = document.getElementById('blur');
+    blur.classList.toggle('active');
+    var clockInM = document.getElementById('clockM');
+    clockInM.classList.toggle('active');
+}
+
 function convertEmpEmail(){
     let employeeID = '1';
     if(userString2 == "adams@tm.com")
@@ -42,29 +60,39 @@ function convertEmpEmail(){
     return employeeID;
 }
 
-function GetClock(){
-    
-    var d=new Date();
-    var nhour=d.getHours(),nmin=d.getMinutes(),nsec=d.getSeconds();
-    if(nmin<=9) nmin="0"+nmin;
-    if(nsec<=9) nsec="0"+nsec;
-    
-    var clocktext=""+nhour+":"+nmin+":"+nsec+"";
-    document.getElementById('clockbox').innerHTML=clocktext;
-    }
-    
-    GetClock();
-    setInterval(GetClock,1000);
+// function checkOpenTimelogs(){
+//     const clockOut = '2012-12-21 03:00:00';
+//     const getClockInsUrl = timelogUrl + "/" + employeeID + "/" + clockOut;
+//     fetch(getClockInsUrl).then(function(response){ 
+//         return response.json();
+//     }).then(function(json) {
+//         timelogList=json;
+//         console.log(timelogList);
+//     }).catch(function(error) {
+//         console.log(error);
+//     });
+// }
 
-    function clockIn(){
-        var blur = document.getElementById('blur');
-        blur.classList.toggle('active');
-        var clockInM = document.getElementById('clockM');
-        clockInM.classList.toggle('active');
+function clockIn(){
 
-        let employeeID = convertEmpEmail();
-        console.log(employeeID);
-        console.log("made it to post for clock-In")
+    let employeeID = convertEmpEmail();
+    console.log(employeeID);
+
+    //first, checking that this user hasn't clock-in already
+    const clockOut = '2012-12-21 03:00:00';
+    const getClockInsUrl = timelogUrl + "/" + employeeID + "/" + clockOut;
+    fetch(getClockInsUrl).then(function(response){ 
+        return response.json();
+    }).then(function(json) {
+        timelogList=json;
+        console.log(timelogList);
+    }).catch(function(error) {
+        console.log(error);
+    });
+    
+    //now, we can create a new timelog entry if we've determined there are no open clock-ins for this user
+    if(timelogList = null)
+    {
         const postClockInApiUrl = timelogUrl;
         const sendClockIn = {
             empID : employeeID,
@@ -81,27 +109,44 @@ function GetClock(){
             myTimelogs = sendClockIn;
             console.log(myTimelogs);
         });
-
-        //how do i get the id if it's auto incrementing... but i need to update this timelog later somehow
-        //do a get at the top to see if that empID already has an entry with the trigger value of clockOut bc that means they've already clocked-In and cant clock in again
-
         var today = new Date();
         var clockInTime = today.getHours() + ":" + today.getMinutes();
         let html = `<div style="color: rgb(170, 9, 9);">${clockInTime}</div>`;
         document.getElementById('displayTimeStamp').innerHTML = html;
-    }
 
-    function clockOut(){
         var blur = document.getElementById('blur');
         blur.classList.toggle('active');
         var clockInM = document.getElementById('clockM');
         clockInM.classList.toggle('active');
+    }
+    else
+    {
+        let html = `<div style="color: rgb(170, 9, 9);">This user has already been clocked in.</div>`;
+        document.getElementById('displayClockInError').innerHTML = html;
+    }  
+}
 
-        //need to get the timelog created above to update the clock-out time
-        //do a get to find the timelogID where the empID is the same and the clockOut value is the trigger value of 2012
-        //
+function clockOut(){
+    
+    let employeeID = convertEmpEmail();
+    console.log(employeeID);
 
-        console.log("made it to post for clock-Out")
+    //grabbing the timelogID of the user's already clocked in session so i can update the clockOut value to now
+    const clockOut = '2012-12-21 03:00:00';
+    const getClockInsUrl = timelogUrl + "/" + employeeID + "/" + clockOut;
+    fetch(getClockInsUrl).then(function(response){ 
+        return response.json();
+    }).then(function(json) {
+        timelogList=json;
+        console.log(timelogList);
+    }).catch(function(error) {
+        console.log(error);
+    });
+    //console.log(timelogList);
+    //console.log(timelogList[0].timelogID);
+
+    if(timelogList != null)
+    {
         const putClockOutApiUrl = timelogUrl;
         const sendClockOut = {
             empID : employeeID,
@@ -115,7 +160,7 @@ function GetClock(){
             body: JSON.stringify(sendClockOut)
         })
         .then((response)=>{
-            myTimelogs = sendClockIn;
+            myTimelogs = sendClockOut;
             console.log(myTimelogs);
         });
 
@@ -123,11 +168,19 @@ function GetClock(){
         var clockOutTime = today.getHours() + ":" + today.getMinutes();
         let html = `<div style="color: rgb(170, 9, 9);">${clockOutTime}</div>`;
         document.getElementById('displayTimeStamp').innerHTML = html;
+
+        var blur = document.getElementById('blur');
+        blur.classList.toggle('active');
+        var clockInM = document.getElementById('clockM');
+        clockInM.classList.toggle('active');
+
+    }
+    else
+    {
+        let html = `<div style="color: rgb(170, 9, 9);">Clock out fail.</div>`;
+        document.getElementById('displayClockInError').innerHTML = html;
+    } 
+
 }
 
-function toggle(){
-    var blur = document.getElementById('blur');
-    blur.classList.toggle('active');
-    var clockInM = document.getElementById('clockM');
-    clockInM.classList.toggle('active');
-}
+
